9914cf7b605038f228a0f42a8f8e9665c0ce725a
YoungwanCho <developer119korea@gmail.com>
2019-11-12 16:47:49 +0900

    프로젝트 생성

---
+++ Assets/SampleScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleScene : MonoBehaviour
+{
+    [SerializeField] private Text _text_ = null;
+
+    private void Start()
+    {
+        _text_.text = "자동 빌드";
+    }
+}
