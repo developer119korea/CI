0793e1fdc1f442ebebbec1342c776bd23eb09773
YoungwanCho <developer119korea@gmail.com>
2019-11-12 17:55:06 +0900

    유니티 플레이어의 메뉴를 통해 빌드 할 수 있도록 수정

---
+++ Assets/Editor/ScriptBatch.cs
@@ -0,0 +1,41 @@
+﻿using UnityEditor;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+public class ScriptBatch
+{
+    [MenuItem("MyTools/Android Build With Postprocess")]
+    public static void BuildGame()
+    {
+        // Get filename.
+        string path = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
+        string[] levels = FindEnabledEditorScenes();
+
+        string buildExportName = string.Format("{0}.apk", UnityEngine.Application.productName);
+
+
+        // Build player.
+        BuildPipeline.BuildPlayer(levels, path + "/" + buildExportName, BuildTarget.Android, BuildOptions.None);
+
+        // Copy a file from the project folder to the build folder, alongside the built game.
+        //FileUtil.CopyFileOrDirectory("Assets/WebPlayerTemplates/Readme.txt", path + "Readme.txt");
+
+        // Run the game (Process class from System.Diagnostics).
+        Process proc = new Process();
+        proc.StartInfo.FileName = path + buildExportName;
+        proc.Start();
+    }
+
+    private static string[] FindEnabledEditorScenes()
+    {
+        List<string> EditorScenes = new List<string>();
+
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            EditorScenes.Add(scene.path);
+        }
+
+        return EditorScenes.ToArray();
+    }
+}
